3d1d3a31698c8f276962efaf6ea0c914aa6f22bd
Роман Петушков <stvoid333@gmail.com>
2022-10-19 21:53:48 +0300

    homework v9

--- homework.py
+++ homework.py
@@ -112,7 +112,7 @@ class Swimming(Training):
         return self.action * self.LEN_STEP / self.M_IN_KM
 
 
-WORKOUT_CODE: dict[str, str] = {
+WORKOUT_CODE: dict[str] = {
     'SWM': Swimming,
     'RUN': Running,
     'WLK': SportsWalking,
